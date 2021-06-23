ff5fb39bca23f707a60caa244e08ac9d5b31e79c
Tony Sahraoui <t.sahraoui@haikara.fr>
2021-06-23 10:00:10 +0200

    MAJ

--- src/DataFixtures/SQL/user_occupation.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `user_occupation` (`id`, `user_id`, `occupation_id`, `is_current`, `is_previous`, `is_desired`) VALUES
-(30, 5, 12, 1, 0, 0),
-(31, 5, 23, 0, 0, 1),
-(32, 5, 30, 0, 1, 0),
-(33, 5, 30, 1, 0, 0),
-(34, 5, 23, 1, 0, 0);
