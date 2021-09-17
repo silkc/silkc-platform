31ae0cea7ae6f4a636984948f8b66be4312c6f8c
Yves Lemée <y.lemee@haikara.fr>
2021-09-17 11:30:08 +0200

    maj js trads

--- assets/js/front/institutional.js
+++ assets/js/front/institutional.js
@@ -491,7 +491,6 @@ class Institutional {
             for (var i = 0; i < inputs.length; i++) {
                 let input = inputs[i];
                 let baseUrl = input.getAttribute('data-url');
-                baseUrl = baseUrl + '/locale/' + lang;
                 let formats = input.getAttribute('data-formats') || 'json';
                 let pagination = input.getAttribute('data-pagination') || false;
                 let params = $.param({'formats': formats, 'pagination': pagination});
